a46b562b24a083407145bac1cf8fd01f95cafa30
Intissar-Ferjani <I.ferjani.26@gmail.com>
2024-11-21 22:07:58 +0100

    first commit

---
+++ lib/entities/classe.dart
@@ -0,0 +1,21 @@
+class Classe {
+  int nbreEtud;
+  String nomClass;
+  //Departement? departement;
+  int? codClass;
+
+  Classe(this.nbreEtud, this.nomClass, [this.codClass]);
+  // Classe(this.nbreEtud, this.nomClass, this.departement, [this.codClass]);
+  //setDepartement(Departement departement) {
+  //this.departement = departement;
+  //}
+
+  Map<String, dynamic> toJson() {
+    return {
+      'codClass': codClass,
+      'nomClass': nomClass,
+      'nbreEtud': nbreEtud,
+      //'departement': departement?.toJson(),
+    };
+  }
+}
